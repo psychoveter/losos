476d8d985ea915bb8308786bbc4e5564056ba913
psychoveter <psychoveter@gmail.com>
2020-11-25 12:30:43 +0300

    scratch of ServiceActionManager

--- process/src/main/kotlin/io/losos/process/planner/ServiceActionManager.kt
+++ process/src/main/kotlin/io/losos/process/planner/ServiceActionManager.kt
@@ -2,10 +2,6 @@ package io.losos.process.planner
 
 import com.fasterxml.jackson.databind.node.ObjectNode
 import io.losos.process.engine.actions.ServiceActionConfig
-import org.slf4j.LoggerFactory
-import org.springframework.http.HttpEntity
-import org.springframework.web.client.RestTemplate
-
 
 interface ServiceActionManager {
 
